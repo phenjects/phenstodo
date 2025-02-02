3420abb23b2ab84e37dfec444adbb8bf069847d4
phen <yusukekitagawa7640@gmail.com>
2025-02-02 23:41:33 +0700

    line break moment:

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const taskInputField = document.getElementById("inputfieldID");
 
 createTask.addEventListener("click", function () {
   const taskCreate = document.createElement("button");
+  const lineBreak = document.createElement("br")
   taskCreate.innerHTML = taskInputField.value;
+  taskCreate.setAttribute("style", "border: none; background-color: black; color: white;")
   document.getElementById("tasksID").appendChild(taskCreate);
+  document.getElementById("tasksID").appendChild(lineBreak)
 });
