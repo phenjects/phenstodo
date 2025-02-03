ffed954c4bccef6e62ec719d639238f756c85624
phen <yusukekitagawa7640@gmail.com>
2025-02-04 01:49:56 +0700

    broken backup no hover

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 const createTask = document.getElementById("newtaskID");
 const taskInputField = document.getElementById("inputfieldID");
 
-createTask.addEventListener("click", function () {
+createTask.onclick = function testFunc() {
   const taskCreate = document.createElement("button");
-  const lineBreak = document.createElement("br")
+  const deleteCreate = document.createElement("button");
+  const lineBreak = document.createElement("br");
   taskCreate.innerHTML = taskInputField.value;
-  taskCreate.setAttribute("style", "border: none; background-color: black; color: white;")
+  taskCreate.setAttribute("style", `border: none; background-color:
+                                    black; color: white;`);
+  
+  deleteCreate.innerHTML = "x"
+  deleteCreate.setAttribute("style", `border-style: solid;
+                                      background-color: black;
+                                      border-color: white;
+                                      color: white;
+                                      width: auto;
+                                      height: auto;
+                                      font-size: 15px;
+                                      border-radius: 7px;`
+                           );
+  deleteCreate.setAttribute("id", `deletebtnID`)
   document.getElementById("tasksID").appendChild(taskCreate);
-  document.getElementById("tasksID").appendChild(lineBreak)
-});
+  document.getElementById("tasksID").appendChild(deleteCreate);
+  document.getElementById("tasksID").appendChild(lineBreak);
+};
+
