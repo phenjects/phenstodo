56133840837ca674c0c1f703c4f21cef3967d10d
phen <yusukekitagawa7640@gmail.com>
2025-02-08 02:02:24 +0700

    (i lied)

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// NOTE: if the code looks ugly, blame prettier.
 
 // INFO: functionality variables.
 const taskInputField = document.getElementById("inputfieldID");
@@ -5,54 +6,63 @@ const taskTags = [];
 
 // INFO: onclick function.
 function addNewTask() {
-
   // INFO: creation variables.
   const fieldset = document.createElement("fieldset");
   const taskCheckbox = document.createElement("input");
-  const delDiv = document.createElement("div");
   const task = document.createElement("input");
   const lineBreak = document.createElement("br");
   const deleteBtn = document.createElement("button");
-  
+
   // INFO: setup before appending.
   task.value = taskInputField.value;
   taskCheckbox.type = "checkbox";
   fieldset.setAttribute("id", `fieldset${taskTags.length}ID`);
-  fieldset.setAttribute("style", "margin-top: 5px; border-radius: 7px; display: flex; flex-direction: row;");
-  task.setAttribute("style", `border: none; background-color: black; color: white;`);
-  delDiv.setAttribute("style", "display: flex; justify-content: end;");
-  
-  // INFO: appends creation variables.
-  document.getElementById("tasksDIV").appendChild(fieldset);
-  document.getElementById(`fieldset${taskTags.length}ID`).appendChild(taskCheckbox);
-  document.getElementById(`fieldset${taskTags.length}ID`).appendChild(task);
-  document.getElementById(`fieldset${taskTags.length}ID`).appendChild(delDiv);
-  document.getElementById(`fieldset${taskTags.length}ID`).appendChild(lineBreak);
+  fieldset.setAttribute(
+    "style",
+    "margin-top: 5px; border-radius: 7px; display: flex; flex-direction: row;"
+  );
+  task.setAttribute(
+    "style",
+    `border: none; background-color: black; color: white;`
+  );
 
-  // INFO: pushes task IDs to tasks array.
+  // INFO: pushes values to arrays.
   taskTags.push(fieldset);
+  taskLabels.push(taskInputField.value);
 
+  // INFO: makes it so that you aren't able to just make a task with an empty label.
+  if (taskInputField.value == "") {
+    window.alert("Please enter a valid task name.");
+  } else {
+    // INFO: appends creation variables.
+    document.getElementById("tasksDIV").appendChild(fieldset);
+    document
+      .getElementById(`fieldset${taskTags.length}ID`)
+      .appendChild(taskCheckbox);
+    document.getElementById(`fieldset${taskTags.length}ID`).appendChild(task);
+    document
+      .getElementById(`fieldset${taskTags.length}ID`)
+      .appendChild(lineBreak);
+  }
   // INFO: deletes task when the delete button is clicked.
   taskTags.forEach((eachTask) => {
-    deleteBtn.onclick = function() {
-      eachTask.remove()
-    }
+    deleteBtn.onclick = function () {
+      eachTask.remove();
+    };
   });
 
-	// INFO: when a task is hovered over, it adds a delete button.
-	fieldset.addEventListener("mouseenter", function () {
-    
+  // INFO: when a task is hovered over, it adds a delete button.
+  fieldset.addEventListener("mouseenter", function () {
     // INFO: sets up attributes of delete button.
     deleteBtn.innerHTML = "Delete";
     deleteBtn.setAttribute("style", "background-color: black; color: white;");
 
     // INFO: appends delete button.
-    delDiv.appendChild(deleteBtn);
-	});
+    fieldset.appendChild(deleteBtn);
+  });
 
   // INFO: when the mouse leaves the task area, it removes the delete button.
-  fieldset.addEventListener("mouseleave", function() {
+  fieldset.addEventListener("mouseleave", function () {
     deleteBtn.remove();
-  })
-
-};
+  });
+}
